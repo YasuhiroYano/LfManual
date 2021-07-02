2575d5b22a53ff21bc4b57ba3f98221788c18d3d
YasuhiroYano <yasu@kkyes.co.jp>
2021-07-02 15:23:53 +0900

    CSSを有効にするためにsphinx.ext.githubpagesを追加

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ release = '1.00 2021/06/30'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.seqdiag','sphinx.ext.todo'
+extensions = ['sphinxcontrib.seqdiag','sphinx.ext.todo', 'sphinx.ext.githubpages'
 ]
 [extensions]
 todo_include_todos=True
